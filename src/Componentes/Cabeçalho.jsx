602d03d049c2d24cbffea9944ace13224ee95ef4
Renato-Marques-Nave <renato.marques@nave.org.br>
2026-04-27 12:00:24 -0300

    salvar

---
+++ src/Componentes/Cabeçalho.jsx
@@ -0,0 +1,13 @@
+import { useState } from react ;
+
+const estilos 
+
+export default function Cabeçalho({
+ paginaInicial, paginaEquipe, mudarPagina
+}) {
+    const [selecionado, setSelecionado] = useState("inicial")
+
+    return(<>  
+        <button></button>
+        </>)
+ }
---
+++ src/componentes/Cabecalho.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react' ;
+
+export default function Cabeçalho({
+ paginaInicial, paginaEquipe, mudarPagina
+}) {
+    const [selecionado, setSelecionado] = useState("inicial")
+
+    return(<>  
+        <button></button>
+        </>)
+ }
